2e136fc97b4853aa769999e1f742bb16c9afd7c0
adityabhartib <adityabharti.ab@gmail.com>
2022-11-04 09:52:34 +0530

    Added :uc2 Ability to peak and pop

--- src/com/bridgelabz/Node.java
+++ src/com/bridgelabz/Node.java
@@ -9,4 +9,24 @@ public class Node <T>{
         next = null;
     }
 
+    public T getData() {
+        return data;
+    }
+
+    public void setData(T data) {
+        this.data = data;
+    }
+
+    public Node getNext() {
+        return next;
+    }
+
+    public void setNext(Node next) {
+        this.next = next;
+    }
+
+    @Override
+    public String toString() {
+        return ""+data;
+    }
 }
